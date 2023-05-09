7ff9714da804dde71d480124c997fce4834e99ad
yul <1103221645@qq.com>
2023-05-10 06:32:59 +0800

    json rpc

---
+++ json_rpc/client/client.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+func main() {
+	conn, _ := net.Dial("tcp", ":8080")
+
+	client := rpc.NewClientWithCodec(jsonrpc.NewClientCodec(conn))
+	var reply string
+	_ = client.Call("HelloService.Hello", "huang", &reply)
+	fmt.Println(reply)
+}
