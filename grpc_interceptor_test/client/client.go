1150f4c57cad0e5065efcb3d004b91e41d0452ff
yul <1103221645@qq.com>
2023-05-12 05:54:08 +0800

    gin getting started

--- grpc_interceptor_test/client/client.go
+++ grpc_interceptor_test/client/client.go
@@ -1,1 +1,13 @@
-package client
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"learn_go/grpc_interceptor_test/proto"
+)
+
+func main() {
+	conn, _ := grpc.Dial("127.0.0.1:8080", grpc.WithInsecure())
+	c := proto.NewGreeterClient(conn)
+	c.SayHello(context.Background(), &proto.HelloRequest{Name: "huang"})
+}
--- grpc_interceptor_test/server/server.go
+++ grpc_interceptor_test/server/server.go
@@ -1,1 +1,37 @@
-package server
+package main
+
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"net"
+)
+import "learn_go/grpc_interceptor_test/proto"
+
+type Server struct{}
+
+func (receiver Server) SayHello(ctx context.Context, request *proto.HelloRequest) (*proto.HelloResponse, error) {
+	return &proto.HelloResponse{
+		Message: "hello, " + request.Name,
+	}, nil
+}
+
+func main() {
+	interceptor := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		// 拦截器的逻辑
+		fmt.Println("receipt new request")
+		// 执行以前的逻辑，相当于next
+		res, err := handler(ctx, req)
+		fmt.Println("请求完成")
+		return res, err
+	}
+	opt := grpc.UnaryInterceptor(interceptor)
+	lis, _ := net.Listen("tcp", ":8080")
+	g := grpc.NewServer(opt)
+	proto.RegisterGreeterServer(g, &Server{})
+	err := g.Serve(lis)
+	if err != nil {
+		return
+	}
+
+}
